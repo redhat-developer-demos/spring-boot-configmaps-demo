2e601af2f5636469ef4687aa1186c0d9395f6842
Kamesh Sampath <kameshsampath@hotmail.com>
2017-09-19 11:46:31 +0530

    Demo setup complete

---
+++ src/main/java/com/redhat/developers/GreeterController.java
@@ -0,0 +1,27 @@
+package com.redhat.developers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class GreeterController {
+
+
+    @Value("${greeter.message}")
+    private String greeterMessageFormat; 
+
+    @GetMapping("/greet/{user}")
+    public String greet(@PathVariable("user") String user) {
+        String prefix = System.getenv().getOrDefault("GREETING_PREFIX", "Hi");
+        log.info("Prefix :{} and User:{}", prefix, user);
+        if (prefix == null) {
+            prefix = "Hello!";
+        }
+
+        return String.format(greeterMessageFormat, prefix, user);
+    }
+}
---
+++ src/main/java/com/redhat/developers/KubernetesSpringBootDemosConfigMapsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesSpringBootDemosConfigMapsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesSpringBootDemosConfigMapsDemoApplication.class, args);
+	}
+}
